8e03678aa6ef1b675dd765b0e4c12710707d3dd4
Ansh Sharma <sansh2356@gmail.com>
2026-04-04 23:44:19 +0530

    feat: adding conditional compilation according to OS archs and build.rs
    fix rust fmt

--- node/src/cli.rs
+++ node/src/cli.rs
@@ -1,11 +1,47 @@
 use clap::Parser;
 use std::path::PathBuf;
 
+/// Default data directory path for Linux
+#[cfg(target_os = "linux")]
+const DEFAULT_DATADIR: &str = "~/.braidpool/";
+
+/// Default data directory path for macOS
+#[cfg(target_os = "macos")]
+const DEFAULT_DATADIR: &str = "~/Library/Application Support/braidpool/";
+
+/// Default data directory path for Windows
+#[cfg(target_os = "windows")]
+const DEFAULT_DATADIR: &str = "%APPDATA%\\braidpool\\";
+
+/// Default Bitcoin RPC cookie path for Linux
+#[cfg(target_os = "linux")]
+const DEFAULT_RPC_COOKIE: &str = "~/.bitcoin/.cookie";
+
+/// Default Bitcoin RPC cookie path for macOS
+#[cfg(target_os = "macos")]
+const DEFAULT_RPC_COOKIE: &str = "~/Library/Application Support/Bitcoin/.cookie";
+
+/// Default Bitcoin RPC cookie path for Windows
+#[cfg(target_os = "windows")]
+const DEFAULT_RPC_COOKIE: &str = "%APPDATA%\\Bitcoin\\.cookie";
+
+/// Default IPC socket path for Linux (Unix domain socket)
+#[cfg(target_os = "linux")]
+const DEFAULT_IPC_SOCKET: &str = "/tmp/bitcoin-cpunet.sock";
+
+/// Default IPC socket path for macOS (Unix domain socket)
+#[cfg(target_os = "macos")]
+const DEFAULT_IPC_SOCKET: &str = "/tmp/bitcoin-cpunet.sock";
+
+/// Default IPC socket path for Windows (named pipe)
+#[cfg(target_os = "windows")]
+const DEFAULT_IPC_SOCKET: &str = "\\\\.\\pipe\\bitcoin-cpunet";
+
 #[derive(Parser, Debug, Clone)]
 #[command(name = "braid", about = "Braidpool Node CLI")]
 pub struct Cli {
     /// Braid data directory
-    #[arg(long, default_value = "~/.braidpool/")]
+    #[arg(long, default_value = DEFAULT_DATADIR)]
     pub datadir: PathBuf,
 
     /// Bind to a given address and always listen on it
@@ -38,10 +74,10 @@ pub struct Cli {
     pub network: Option<String>,
 
     /// Use this cookie file for bitcoin RPC
-    #[arg(long, default_value = "~/.bitcoin/.cookie")]
+    #[arg(long, default_value = DEFAULT_RPC_COOKIE)]
     pub rpccookie: Option<String>,
 
-    /// Path to Bitcoin Core IPC socket
-    #[arg(long, default_value = "/tmp/bitcoin-cpunet.sock")]
+    /// Path to Bitcoin Core IPC socket (Unix domain socket on Linux/macOS, named pipe on Windows)
+    #[arg(long, default_value = DEFAULT_IPC_SOCKET)]
     pub ipc_socket: String,
 }
